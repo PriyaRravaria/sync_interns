0881fb2859940684b4b4ef1373920311bd40ee85
PriyaRravaria <priya.ravaria01@gmail.com>
2023-08-19 10:48:23 +0530

    fist commit

---
+++ music_player_task_3/js/music-list.js
@@ -0,0 +1,33 @@
+let allMusic = [
+  {
+    name: "Karpur Gauram Karunavtaram",
+    artist: "Abhilipsa Panda",
+    img: "Karpur Gauram Karunavtaram",
+    src: "Karpur Gauram Karunavtaram"
+  },
+  {
+    name: "Laagi Lagan Shankara",
+    artist: "Hansraj Raghuvanshi",
+    img: "Laagi Lagan Shankara",
+    src: "Laagi Lagan Shankara"
+  },
+  {
+    name: "Parvati Boli Shankar Se",
+    artist: "Hansraj Raghuvanshi",
+    img: "Parvati Boli Shankar Se",
+    src: "Parvati Boli Shankar Se"
+  },
+  {
+    name: "Raatan Lambiyan",
+    artist: "Jubin Nautiyal",
+    img: "Raatan Lambiyan",
+    src: "Raatan Lambiyan"
+  },
+  {
+    name: "Tu Hai To Mujhe Phir Aur Kya Chahiye",
+    artist: "Amitabh Bhattacharya, Arijit Singh",
+    img: "Tu Hai To Mujhe Phir Aur Kya Chahiye",
+    src: "Tu Hai To Mujhe Phir Aur Kya Chahiye"
+  },
+  
+];
